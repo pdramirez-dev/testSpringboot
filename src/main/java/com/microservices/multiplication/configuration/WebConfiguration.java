d9161d4ed5ecab231fdad59e354e51bdf0bb391b
Paul <pdramirez.h@gmail.com>
2023-03-09 22:05:52 -0500

    add new clases

---
+++ src/main/java/com/microservices/multiplication/configuration/WebConfiguration.java
@@ -0,0 +1,8 @@
+package com.microservices.multiplication.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebConfiguration  implements WebMvcConfigurer {
+
+}
